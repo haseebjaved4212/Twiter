bde1ece958cc8b0e194eeb9e423968b731ffe1e4
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-19 21:27:15 +0500

    feat: implement index, tweet list, and tweet creation views

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Tweet
 # pyrefly: ignore [missing-import]
 from .forms import TweetForm
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404 , redirect
 # Create your views here. 
 
 
@@ -15,3 +15,16 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     
     return render(request, 'tweet_list.html', {'tweets': tweets})
+
+
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet =  form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
