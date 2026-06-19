950d0e347f58fcf9e25c470b9ddec48d18e722fd
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-19 21:21:25 +0500

    feat: register Tweet model in admin and implement tweet listing view

--- chaiheadq/tweet/admin.py
+++ chaiheadq/tweet/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
 from .models import Tweet
+
 
 # Register your models here.
 
--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
+# pyrefly: ignore [missing-import]
+from .models import Tweet
+# pyrefly: ignore [missing-import]
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404
+# Create your views here. 
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    
+    return render(request, 'tweet_list.html', {'tweets': tweets})
