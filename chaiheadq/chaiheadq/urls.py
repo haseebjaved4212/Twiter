e0bafb9c1e89630dc8807f1241477703680e02da
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-19 11:42:42 +0500

    feat: initialize project URL configuration and base index template

--- chaiheadq/chaiheadq/urls.py
+++ chaiheadq/chaiheadq/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include
 from django.conf import settings
 from django.conf.urls import static
 from django.contrib import admin
@@ -21,5 +22,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tweet/', include('tweet.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
