28521803ad2beec35e718141fc4fa1ac3034d90a
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-19 11:21:26 +0500

    feat: initialize tweet app with base configuration, url routing, and index view

--- chaiheadq/tweet/urls.py
+++ chaiheadq/tweet/urls.py
@@ -1,8 +1,10 @@
-from django.conf import settings
-from django.conf.urls import static
-from django.contrib import admin
+
 from django.urls import path
+from django import dispatch
+from . import views
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
