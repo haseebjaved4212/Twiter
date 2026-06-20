3db86523306e1735b5a454c0357f86f6820eeaf5
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-20 20:20:21 +0500

    feat: add TweetForm and UserRegistrationForm to handle tweet creation and user signup

--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -1,7 +1,17 @@
+from django.contrib.auth.models import User
 from django import forms
+# pyrefly: ignore [missing-import]
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
