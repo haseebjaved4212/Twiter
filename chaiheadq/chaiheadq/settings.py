57ed2ee650b392b11fbdd4eb57e0ec85cb5fabfc
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-20 20:36:48 +0500

    feat: initialize project settings and define tweet application URL routing

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -122,3 +122,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/tweet/'
+LOGOUT_REDIRECT_URL = '/tweet/'
