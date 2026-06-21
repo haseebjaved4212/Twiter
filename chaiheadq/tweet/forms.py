2940dfd134b48cfa33e95b158954dfb3be02cf8c
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-21 08:43:46 +0500

    feat: add Tweet and User registration forms to tweet application

--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -11,7 +11,8 @@ class TweetForm(forms.ModelForm):
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
+    # password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('username', 'email', 'password')
